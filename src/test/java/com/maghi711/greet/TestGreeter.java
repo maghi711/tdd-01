46d9bd7275d9127f9c81f163a0a5d749c2a964d9
Mahesh J <maghi711@gmail.com>
2020-06-07 08:22:18 +0530

    Initial commit

---
+++ src/test/java/com/maghi711/greet/TestGreeter.java
@@ -0,0 +1,12 @@
+package com.maghi711.greet;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestGreeter {
+
+    @Test
+    public void greetHello() {
+        Assert.fail("No greeting available yet");
+    }
+}
